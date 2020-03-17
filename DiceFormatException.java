e839264d09d3a7b37f3a1a6fa8779fbac64f2240
Jessica Gage <jemgage@gmail.com>
2020-03-16 20:44:24 -0600

    Fixing GitHub merges.

--- DiceFormatException.java
+++
@@ -1,9 +0,0 @@
-public class DiceFormatException extends Exception{
-    public DiceFormatException(String errorMessage){
-        super(errorMessage);
-
-        errorMessage = ("Sorry, your dice format was invalid. Please try again.");
-
-        System.out.println(errorMessage);
-    }
-}
---
+++ Tests/TestCharacterBuilder.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCharacterBuilder{
+    @Test
+    public void testPlayerRace(){
+
+    }
+}
